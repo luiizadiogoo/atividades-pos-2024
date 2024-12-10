87187c008a3aec63147ff51f40feac2b5696b900
luiizadiogoo <l.diogo@escolar.ifrn.edu.br>
2024-12-10 19:55:17 -0300

    API PRONTA

--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import viewsets
+from .models import Artista, Album, Musica
+from .serializers import ArtistaSerializer, AlbumSerializer, MusicaSerializer
+
+class ArtistaViewSet(viewsets.ModelViewSet):
+    queryset = Artista.objects.all()
+    serializer_class = ArtistaSerializer
+
+class AlbumViewSet(viewsets.ModelViewSet):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+class MusicaViewSet(viewsets.ModelViewSet):
+    queryset = Musica.objects.all()
+    serializer_class = MusicaSerializer
